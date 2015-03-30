225821c0f93b39a93d5e180549ade2e94e62a128
amits <amits@PRINHYDSPLN0153>
2015-03-30 09:58:45 +0530

    Adding crawler service interface

--- webCrawler/src/main/java/com/organisation/pramati/webCrawler/services/CrawlerService.java
+++ webCrawler/src/main/java/com/organisation/pramati/webCrawler/services/CrawlerService.java
@@ -1,42 +1,5 @@
 package com.organisation.pramati.webCrawler.services;
 
-
-import java.util.Set;
-
-import com.organisation.pramati.webCrawler.services.serviceProcessor.CrawlerServiceProcessor;
-
-public class CrawlerService {
-	
-	public CrawlerServiceProcessor crawlerServiceProcessorObj;
-	
-	public void downloadMailService(String mailYear)
-	
-	{
-		crawlerServiceProcessorObj = new CrawlerServiceProcessor();
-		
-		Set<String> hyperLinksOfMonths = crawlerServiceProcessorObj.getHyperlinksOfGivenYearService(mailYear);
-		
-		if(hyperLinksOfMonths!=null && hyperLinksOfMonths.size()>0)
-		{
-			
-			Set<String> hyperLinksOfAllMails= crawlerServiceProcessorObj.getHyperLinksOfAllMonthsMails(hyperLinksOfMonths);
-			
-			/*if(hyperLinksOfAllMails!=null && hyperLinksOfAllMails.size()>0)
-				crawlerServiceProcessorObj.getViewRawMessageAndSaveEmails(hyperLinksOfAllMails);
-			else
-				System.out.println("No mails to save");*/
-				
-		}
-		
-		else
-			
-			System.out.println("No mails exist for the year");
-		
-		
-	}
-	
-	
-		
-	
+public interface CrawlerService {
 
 }
